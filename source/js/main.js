39a402998d5f5cb8c22b9c8df9a23d6f2857bc6b
Kapitonov Roman <kapitroman@yandex.ru>
2023-09-06 12:32:59 +0300

    верстка секции faq

--- source/js/main.js
+++ source/js/main.js
@@ -4,6 +4,8 @@ import {Form} from './modules/form-validate/form';
 import {CustomSelect} from './modules/select/custom-select';
 import {uploadFile, uploadImageDrop} from './modules/input-file/init-upload';
 
+import {initAccordions} from './modules/init-accordion';
+
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -27,6 +29,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const form = new Form();
     window.form = form;
     form.init();
+    initAccordions();
   });
 });
 
